516b8d882c18149cdebb141e9d403c9dd4e8fd36
Stanislav Knot <knot.stanislav@gmail.com>
2016-12-02 12:00:44 +0100

    Initial commit - working parts

---
+++ parts/triangulation/build/CMakeFiles/triangulate.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/triangulate.dir/triangulate.cpp.o"
+  "triangulate.pdb"
+  "triangulate"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/triangulate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
