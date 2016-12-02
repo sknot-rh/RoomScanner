bf2bc3561a7ae9e971456e3136e30841ac8792c2
stanlyDoge <sknot@redhat.com>
2016-12-02 15:58:32 +0100

    Revert "more cleaning"

--- parts/triangulation/build/cmake_install.cmake
+++ parts/triangulation/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/stanly/kinect/DP/RoomScanner/parts/triangulation
+# Install script for directory: /home/stanly/kinect/DP/triangulation
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/stanly/kinect/DP/RoomScanner/parts/triangulation/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/stanly/kinect/DP/triangulation/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
