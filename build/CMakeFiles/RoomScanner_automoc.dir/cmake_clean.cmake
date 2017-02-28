f88b9357f40e3b935586ab251896eb7d3e14e8ae
stanlyDoge <knot.stanislav@gmail.com>
2017-02-28 13:32:25 +0100

    clear build folder

--- build/CMakeFiles/RoomScanner_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/RoomScanner.dir/qrc_Resources.cpp"
-  "CMakeFiles/RoomScanner_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/RoomScanner_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
